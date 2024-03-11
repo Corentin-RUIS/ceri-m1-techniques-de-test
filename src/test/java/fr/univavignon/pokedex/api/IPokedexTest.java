f8de743b2178c1cd93e8005e3bb54de1c3ea857f
Corentin <corentin.ruis@alumni.univ-avignon.fr>
2024-03-11 11:17:32 +0100

    sans erreur

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -18,47 +18,4 @@ public class IPokedexTest {
     public Pokemon pokemon = new Pokemon(3, "Jules", 3, 2000, 1,
             0, 1000, 4, 2, 2);
 
-    @Before
-    public void setUp() throws PokedexException {
-        pokedex = mock(IPokedex.class);
-        when(pokedex.getPokemons()).thenReturn((List<Pokemon>) when(pokedex.getPokemons()).thenReturn(Arrays.asList(
-                new Pokemon(0, "Bulbizarre", 120, 126, 90, 0, 100, 0, 0, 0),
-                new Pokemon(1, "Salamèche", 126, 126, 90, 0, 150, 0, 0, 0),
-                new Pokemon(2, "Kaiminus", 220, 126, 55, 0, 200, 0, 0, 0)
-        )));
-
-        when (pokedex.getPokemon(0)).thenReturn(
-            new Pokemon(0, "Bulbizarre", 126, 126, 90, 0, 0, 0, 0, 0)
-        );
-    }
-
-    @Test
-    public void testGetPokemon() throws PokedexException {
-        PokemonMetadata metadata = pokedex.getPokemon(0);
-        assertEquals("Bulbizarre", metadata.getName());
-        assertEquals(126, metadata.getAttack());
-        assertEquals(126, metadata.getDefense());
-        assertEquals(90, metadata.getStamina());
-    }
-
-    @Test
-    public void testGetPokemons() {
-        List<Pokemon> pokemons = pokedex.getPokemons();
-        assertEquals(3, pokemons.size());
-        assertEquals("Bulbizarre", pokemons.get(0).getName());
-        assertEquals("Salamèche", pokemons.get(1).getName());
-        assertEquals("Kaiminus", pokemons.get(2).getName());
-        assertEquals(55, pokemons.get(2).getStamina());
-    }
-
-    @Test
-    public void testGetPokemonsSorted() {
-        List<Pokemon> pokemons = pokedex.getPokemons();
-        Comparator<Pokemon> comparator = Comparator.comparing(Pokemon::getName);
-        List<Pokemon> sortedPokemons = pokemons.stream().sorted(comparator).collect(Collectors.toList());
-
-        assertEquals("Bulbizarre", sortedPokemons.get(0).getName());
-        assertEquals("Kaiminus", sortedPokemons.get(1).getName());
-        assertEquals("Salamèche", sortedPokemons.get(2).getName());
-    }
 }
