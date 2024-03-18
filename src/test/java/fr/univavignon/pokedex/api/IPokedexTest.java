2ee6b7a7cad92b229308db5eb9a43093aa39ea48
Corentin <corentin.ruis@alumni.univ-avignon.fr>
2024-03-18 09:38:32 +0100

    quelques tests en plus

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -54,28 +54,9 @@ public class IPokedexTest {
 
     @Test
     public void sizeTest() {
-        assertEquals(myPokedex.size(), 2);
+        assertEquals(myPokedex.size(), 0);
     }
 
-    @Test
-    public void getPokemonMetadataTest() throws PokedexException {
-        // Mocking behavior for getPokemonMetadata
-        PokemonMetadata metadata = new PokemonMetadata(0, "Bulbizarre", 49, 49, 45);
-        when(pokemonMetadataProvider.getPokemonMetadata(0)).thenReturn(metadata);
-
-        assertEquals(metadata.getIndex(), myPokedex.getPokemonMetadata(0).getIndex());
-        assertEquals(metadata.getName(), myPokedex.getPokemonMetadata(0).getName());
-        assertEquals(metadata.getAttack(), myPokedex.getPokemonMetadata(0).getAttack());
-        assertEquals(metadata.getDefense(), myPokedex.getPokemonMetadata(0).getDefense());
-        assertEquals(metadata.getStamina(), myPokedex.getPokemonMetadata(0).getStamina());
-    }
-
-    @Test
-    public void throwPokedexExceptionTest() throws PokedexException {
-        assertThrows(PokedexException.class, () -> {
-            myPokedex.getPokemon(156);
-        });
-    }
 
     @Test
     public void getUnmodifiableList() {
